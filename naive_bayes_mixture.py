a83e1725cfa306347d0b2edf1668b51bd918e9ef
Scott Huang <mingwei6@illinois.edu>
2021-02-22 19:52:50 -0600

    first try on part 2

--- naive_bayes_mixture.py
+++ naive_bayes_mixture.py
@@ -14,8 +14,8 @@ within this file for Part 2 -- the unrevised staff files will be used for all ot
 files and classes when code is run, so be careful to not modify anything else.
 """
 
-
-
+from collections import Counter
+from math import log
 
 def naiveBayesMixture(train_set, train_labels, dev_set, bigram_lambda,unigram_smoothing_parameter, bigram_smoothing_parameter, pos_prior):
     """
@@ -42,6 +42,81 @@ def naiveBayesMixture(train_set, train_labels, dev_set, bigram_lambda,unigram_sm
     # TODO: Write your code here
     # return predicted labels of development set
 
+    # get prob for unigram
+    spam_uniwords = Counter()
+    email_uniwords = Counter()
+    for words, label in zip(train_set, train_labels):
+        if label:
+            email_uniwords.update(words)
+        else:
+            spam_uniwords.update(words)
 
+    # get the total uni words number
+    spam_uniwords_num = sum(spam_uniwords.values())
+    email_uniwords_num = sum(email_uniwords.values())
 
-    return []
+    spam_biwords = Counter()
+    email_biwords = Counter()
+    for words, label in zip(train_set, train_labels):
+        bigrams = zip(words[:-1], words[1:])
+        if label:
+            email_biwords.update(bigrams)
+        else:
+            spam_biwords.update(bigrams)
+
+    # get the total bi words number
+    spam_biwords_num = sum(spam_biwords.values())
+    email_biwords_num = sum(email_biwords.values())
+    #print("uni num ", len(spam_uniwords), len(email_uniwords))
+    #print("bi num ", len(spam_biwords), len(email_biwords))
+    #print(email_biwords.most_common(30))
+    #print(email_uniwords.most_common(30))
+
+    # function for probability of P(biword | class)   
+    def email_uniprob(word):
+        return (email_uniwords[word] + unigram_smoothing_parameter) / (email_uniwords_num + unigram_smoothing_parameter * len(email_uniwords))
+    def spam_uniprob(word):
+        return (spam_uniwords[word] + unigram_smoothing_parameter) / (spam_uniwords_num + unigram_smoothing_parameter * len(spam_uniwords))
+    def email_biprob(biword):
+        return (email_biwords[biword] + bigram_smoothing_parameter) / (email_biwords_num + bigram_smoothing_parameter * len(email_biwords))
+    def spam_biprob(biword):
+        return (spam_biwords[biword] + bigram_smoothing_parameter) / (spam_biwords_num + bigram_smoothing_parameter * len(spam_biwords))
+
+    # function for calculate probability of P(class | words)
+    def email_uni(words):
+        ret = log(pos_prior)
+        for word in words:
+            ret += log(email_uniprob(word))
+        return ret
+    def spam_uni(words):
+        ret = log(1-pos_prior)
+        for word in words:
+            ret += log(spam_uniprob(word))
+        return ret
+    def email_bi(words):
+        ret = log(pos_prior)
+        bigrams = zip(words[:-1], words[1:])
+        for word_pair in bigrams:
+            ret += log(email_biprob(word_pair))
+        return ret
+    def spam_bi(words):
+        ret = log(1-pos_prior)
+        bigrams = zip(words[:-1], words[1:])
+        for word_pair in bigrams:
+            ret += log(spam_biprob(word_pair))
+        return ret
+
+    # functions calculating probability for each class
+    def email_prob(words):
+        return bigram_lambda * email_bi(words)# + (1-bigram_lambda) * email_uni(words)
+    def spam_prob(words):
+        return bigram_lambda * spam_bi(words)# + (1-bigram_lambda) * spam_uni(words)
+
+    # applay the trained model on the testing set
+    ret = []
+    for words in dev_set:
+        if email_uni(words) > spam_uni(words):
+            ret.append(1)
+        else:
+            ret.append(0)
+    return ret
